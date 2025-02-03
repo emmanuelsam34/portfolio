ddc7f07c3ac11d8922565c6f5abf3c0ffe063aa0
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-02-03 16:43:08 +0100

    changes

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -55,7 +55,7 @@ const home = {
   headline: <>Software Engineer</>,
   subline: (
     <>
-      I'm Emmanuel, a software engineer at <InlineCode>NoName</InlineCode>, where I craft intuitive
+      I'm Emmanuel, a software engineer at <InlineCode>FLY</InlineCode>, where I craft intuitive
       <br /> user experiences. After hours, I build my own projects.
     </>
   ),
@@ -81,7 +81,7 @@ const about = {
     title: "Introduction",
     description: (
       <>
-        Emmanuel is a Lagos-based design engineer with a passion for transforming complex challenges
+        Emmanuel is a Lagos-based software engineer with a passion for transforming complex challenges
         into simple, elegant design solutions. His work spans digital interfaces, interactive
         experiences, and the convergence of design and technology.
       </>
@@ -210,37 +210,37 @@ const gallery = {
   // Images from https://pexels.com
   images: [
     {
-      src: "/images/gallery/img-01.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/img-02.jpg",
+      src: "/images/gallery/7321 (1).jpg",
       alt: "image",
       orientation: "horizontal",
     },
     {
-      src: "/images/gallery/img-03.jpg",
+      src: "/images/gallery/7321 (1).jpg",
       alt: "image",
       orientation: "vertical",
     },
     {
-      src: "/images/gallery/img-04.jpg",
+      src: "/images/gallery/7321 (1).jpg",
       alt: "image",
       orientation: "horizontal",
     },
     {
-      src: "/images/gallery/img-05.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-06.jpg",
+      src: "/images/gallery/7321 (1).jpg",
       alt: "image",
       orientation: "vertical",
     },
     {
-      src: "/images/gallery/img-07.jpg",
+      src: "/images/gallery/7321 (1).jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/7321 (1).jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/7321 (1).jpg",
       alt: "image",
       orientation: "horizontal",
     },
