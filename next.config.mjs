49a850a3e739ea3fc480f0a579e7316524ca382e
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-04-03 15:38:58 +0100

    image changes

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  images: {
+    domains: [],
+    unoptimized: true,
+  },
 };
 
 export default withMDX(nextConfig);
